48dfc8c0041a2c58c6c767e6b0b440396d937b49
Daniela Montelo <dani.montelo@gmail.com>
2018-09-21 23:36:35 -0300

    Arquivo do exercício 1
    Exercício 1 - informar valor da velocidade e verificar limite e calcular multa para o excesso de velocidade.

---
+++ velocidade.py
@@ -0,0 +1,8 @@
+velocidade = float(input("Entre com o valor da velocidade:"))
+
+if velocidade > 80: 
+    print("Você foi multado por excesso de velocidade!")
+    multa = ((velocidade - 80) * 5)
+    print('Valor da Multa: R$',multa)
+else:
+    print("Você está dentro do limite de velocidade permitida!")
